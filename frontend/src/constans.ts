a172a916be51a28b3b3a58af8fe3989ca5cb4341
Abhinand K <abhinand8413@gmail.com>
2024-08-21 23:02:53 +0530

    Fix: changed the front end point

--- frontend/src/constans.ts
+++ frontend/src/constans.ts
@@ -1,4 +1,4 @@
 // export const ENDPOINT = "http://localhost:8000"
 export const ENDPOINT = "https://drop-api-30ro.onrender.com"
 
-export const FRONTENDPOINT = "https://drop-ebon.vercel.app"
+export const FRONTENDPOINT = "https://drop-me.vercel.app"
