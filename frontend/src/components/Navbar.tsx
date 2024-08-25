4fd4675f6d36725b2758ea626db6b981500f391d
Abhinand K <abhinand8413@gmail.com>
2024-08-25 12:07:55 +0530

    Feat: add theme mode icon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
-// import ModeNightOutlinedIcon from '@mui/icons-material/ModeNightOutlined';
+import ModeNightOutlinedIcon from '@mui/icons-material/ModeNightOutlined';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className='text-white h-14 flex justify-between px-5 items-center '>
         <h1 className='text-3xl'><Link to='/'>drop.me</Link></h1>
 
-        {/* <ModeNightOutlinedIcon className='cursor-pointer'/> */}
+        <ModeNightOutlinedIcon className='cursor-pointer'/>
 
     </div>
   )
